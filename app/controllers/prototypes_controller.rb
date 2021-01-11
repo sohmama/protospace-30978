4571678c965231152de21d05451638d78195b84c
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-01-11 22:48:50 +0900

    投稿者以外プロトタイプ編集ページに遷移できないよう修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -44,16 +44,13 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-private
+  private
 
-def prototype_params
-  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id:current_user.id)
-end
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id:current_user.id)
   end
-end
 
+  def move_to_index
+      redirect_to root_path unless current_user == @prototype
+  end
 end
